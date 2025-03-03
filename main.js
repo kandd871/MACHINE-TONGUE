f54b547bee2e65b6b2d4291cab24307aad53bd3b
kandd871 <114494325+kandd871@users.noreply.github.com>
2025-03-02 21:44:31 -0500

    media query fixed

--- main.js
+++ main.js
@@ -144,6 +144,7 @@ function setup() {
     userStartAudio().then(() => {
       mic.start();
       console.log("MOBILE DETECTED: MICROPHONE AUTO-STARTED");
+      machineTongueInitiated = true; 
     }).catch(e => {
       console.error("Failed to start audio:", e);
     });
